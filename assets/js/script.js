3fa0cdafab0115220e771695cff5481edea1b9b4
FernandoJP21 <95588305+FernandoJP21@users.noreply.github.com>
2024-09-03 10:28:48 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 $(document).ready(function () {
 
-    // toggle mobile menu
     $('[data-toggle="toggle-nav"]').on('click', function () {
         $(this).closest('nav').find($(this).attr('data-target')).toggleClass('hidden');
         return false;
     });
 
-    // feather icons
     feather.replace();
 
-    // smooth scroll
     var scroll = new SmoothScroll('a[href*="#"]');
 
-    // tiny slider
     $('#slider-1').slick({
         infinite: true,
         prevArrow: $('.prev'),
